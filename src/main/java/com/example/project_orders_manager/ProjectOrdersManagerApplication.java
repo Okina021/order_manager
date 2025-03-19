1615ca33c53879f39a7f8344b9589ab9893166ff
Gustavo Henrique Amaral <gustavo@integra.do>
2025-03-19 08:23:16 -0300

    change name

--- src/main/java/com/example/project_orders_manager/ProjectOrdersManagerApplication.java
+++ src/main/java/com/example/project_orders_manager/ProjectOrdersManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProvaEvoApplication {
+public class ProjectOrdersManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProvaEvoApplication.class, args);
+		SpringApplication.run(ProjectOrdersManagerApplication.class, args);
 	}
 
 }
